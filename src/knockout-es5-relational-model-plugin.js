73e8e0418c1c0bd03ad65383ab7d6c1ceca2e2f3
asaake <asaake88@gmail.com>
2014-04-02 16:11:31 +0900

    bowerの設定を追加

---
+++ src/knockout-es5-relational-model-plugin.js
@@ -0,0 +1,27 @@
+Model.prototype.setProperty = function(name, value, expect) {
+    var expectAttrs;
+    if (expect == null) {
+        expect = true;
+    }
+    expectAttrs = this.constructor.expectAttrs();
+    if (!(expectAttrs.isEmpty())) {
+        if (expectAttrs.indexOf(name) !== -1) {
+            this.attrs()[name] = value;
+            return ko.track(this.attrs(), [name]);
+        }
+    } else {
+        this.attrs()[name] = value;
+        return ko.track(this.attrs(), [name]);
+    }
+};
+
+Model.prototype.getObservable = function(name, expect) {
+    if (expect == null) {
+        expect = true;
+    }
+    if (expect && !this.hasProperty(name)) {
+        throw new Error("" + (this.getClassName()) + " has not " + name + " property.\n    attrs: " + (JSON.stringify(this.attrs())));
+    } else {
+        return ko.getObservable(this.attrs(), name);
+    }
+};
